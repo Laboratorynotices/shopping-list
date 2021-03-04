76a61058db4e355bbb50ad71fb0e27fc6cf9f214
Andrey Smirnov <muenchen.webas@gmail.com>
2021-03-04 12:51:56 +0100

    Создание списка покупок

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,6 +1,29 @@
 <template>
   <div class="home pa-6">
-    <h1> Your shopping list </h1>
+    <v-list
+      subheader
+      flat
+    >
+
+      <v-list-item
+        v-for="item in shoppingList"
+        :key="item.id"
+      >
+        <template v-slot:default="{ active, }">
+          <v-list-item-action>
+            <v-checkbox
+              :input-value="active"
+              color="primary"
+            ></v-checkbox>
+          </v-list-item-action>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }} </v-list-item-title>
+          </v-list-item-content>
+        </template>
+      </v-list-item>
+
+    </v-list>
   </div>
 </template>
 
@@ -10,6 +33,22 @@
 
 export default {
   name: 'List',
+  data: () => ({
+    shoppingList: [
+      {
+        id: 1,
+        title: 'Muesli'
+      },
+      {
+        id: 2,
+        title: 'Milk'
+      },
+      {
+        id: 3,
+        title: 'Aple'
+      }
+    ]
+  }),
   components: {
   }
 }
