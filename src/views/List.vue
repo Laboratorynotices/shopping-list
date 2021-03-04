e1ec0c8d477965478eda9fb8db4eede968b84255
Andrey Smirnov <muenchen.webas@gmail.com>
2021-03-04 21:49:03 +0100

    Добавляем новый элемент

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="home pa-6">
+    <v-text-field
+      outlined
+      label="Add item"
+      append-icon="mdi-cart-plus"
+      clearable
+      v-bind="newItemTitle"
+      @click:append="addNewItem"
+      @keyup.enter="addNewItem"
+    />
     <v-list
       subheader
       flat
@@ -59,6 +68,7 @@
 export default {
   name: 'List',
   data: () => ({
+    // Список покупок по умолчанию
     shoppingList: [
       {
         id: 1,
@@ -75,21 +85,44 @@ export default {
         title: 'Aple',
         bought: false
       }
-    ]
+    ],
+    // Переменная для создания новых элементов из списка
+    newItemTitle: ''
   }),
   components: {
   },
   methods: {
+    /**
+     * Отмечаем элемент как купленным.
+     */
     doneBought (id) {
       // в списке покупок ищем нужную запись
       const item = this.shoppingList.filter(item => item.id === id)[0]
       // Изменяем статус покупки
       item.bought = !item.bought
     },
+    /**
+     * Удаляем элемент из списка
+     */
     deleteItem (id) {
       // перебираем все элементы в списке и смотрим,
       // чтобы id его не сходился с той, что надо убрать.
       this.shoppingList = this.shoppingList.filter(item => item.id !== id)
+    },
+    /**
+     * Добавляем новый элемент
+     */
+    addNewItem () {
+      // Создаём новый элемент с правильной структурой
+      const newItem = {
+        id: Date.now(),
+        title: this.newItemTitle,
+        bought: false
+      }
+      // добавляем созданный элемент к уже существующему списку
+      this.shoppingList.push(newItem)
+      // очищаем поле ввода
+      this.newItemTitle = ''
     }
   }
 }
