b218c083d58ebcff7cb1a56580eff8e56e5b116f
Andrey Smirnov <muenchen.webas@gmail.com>
2021-03-04 02:45:19 +0100

    Страничка About

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>Vuetify shopping list</h1>
+    <p>It is a simple example page of the Vuetify.</p>
+    <h2>Don't forget to bye anymore.</h2>
+    <p>This will help you remember what to buy. A simple list. With the option to indicate the last date of purchase.</p>
   </div>
 </template>
