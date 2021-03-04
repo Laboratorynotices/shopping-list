8dddd4d5beb45477d8ddeb140fbc78592df8855c
Andrey Smirnov <muenchen.webas@gmail.com>
2021-03-04 13:21:33 +0100

    Разделитель

--- src/views/List.vue
+++ src/views/List.vue
@@ -5,23 +5,26 @@
       flat
     >
 
-      <v-list-item
+      <div
         v-for="item in shoppingList"
         :key="item.id"
       >
-        <template v-slot:default="{ active, }">
-          <v-list-item-action>
-            <v-checkbox
-              :input-value="active"
-              color="primary"
-            ></v-checkbox>
-          </v-list-item-action>
+        <v-list-item>
+          <template v-slot:default="{ active, }">
+            <v-list-item-action>
+              <v-checkbox
+                :input-value="active"
+                color="primary"
+              />
+            </v-list-item-action>
 
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }} </v-list-item-title>
-          </v-list-item-content>
-        </template>
-      </v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }} </v-list-item-title>
+            </v-list-item-content>
+          </template>
+        </v-list-item>
+        <v-divider />
+      </div>
 
     </v-list>
   </div>
