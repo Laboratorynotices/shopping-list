e16be6740dd1c73a1b4536a11c480d96d96d9512
Andrey Smirnov <muenchen.webas@gmail.com>
2021-03-04 11:23:54 +0100

    Немного поменял машрутизацию

---
+++ src/views/List.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home pa-6">
+    <h1> Your shopping list </h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'List',
+  components: {
+  }
+}
+</script>
